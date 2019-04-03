9c987ffed6c4554332047b652e48cde5a8640dab
mrpetrov <>
2019-04-03 13:26:50 +0300

    Beautify build script
    Make it more RGB, so to speak...

--- build.sh
+++ build.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
+echo "Checking if solard.c is present..."
 if [ ! -e solard.c ]
 then
-    echo Refusing to start from non-source-root dir...
-    echo ERROR: No solard.c found in `pwd`!
+    echo "$(tput setaf 1)$(tput setab 7)Refusing to start from non-source-root dir...$(tput sgr 0)"
+    echo "$(tput setaf 1)$(tput setab 7)ERROR: No solard.c found in `pwd`!$(tput sgr 0)"
     exit 2
 fi
+echo "$(tput setaf 2)OK.$(tput sgr 0)"
 
 if [ -e solard ]
 then
     rm solard
+	echo "$(tput setaf 3)Previous compile result was still present. It is now removed.$(tput sgr 0)"
 fi
-date
-time gcc -D_FORTIFY_SOURCE=2 -DSOLARDVERSION=\"`git describe --tag`\" -Wall -Wno-unused-result -O3 -o solard solard.c
-ls -l
+if ! gcc -D_FORTIFY_SOURCE=2 -DSOLARDVERSION=\"`git describe --tag`\" -Wall -Wno-unused-result -O3 -o solard solard.c; then
+	echo "$(tput setaf 1)$(tput setab 7)ERROR: Compilation failed!$(tput sgr 0)"
+	exit 3
+else
+	echo "$(tput setaf 2)Success.$(tput sgr 0)"
+	echo "$(tput setaf 2)Compile time and date: $(tput setab 7)`date`$(tput sgr 0)"
+fi
 
 #EOF
