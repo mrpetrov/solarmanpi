05029925c160b183b21241ed6a5a2177865c4884
mrpetrov <>
2019-04-03 13:34:39 +0300

    Last touches to build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "Checking if solard.c is present..."
 if [ ! -e solard.c ]
 then
     echo "$(tput setaf 1)$(tput setab 7)Refusing to start from non-source-root dir...$(tput sgr 0)"
-    echo "$(tput setaf 1)$(tput setab 7)ERROR: No solard.c found in `pwd`!$(tput sgr 0)"
+    echo "$(tput setaf 1)$(tput setab 7)ERROR: No solard.c found in $(tput smso)`pwd`$(tput smso)!$(tput sgr 0)"
     exit 2
 fi
 echo "$(tput setaf 2)OK.$(tput sgr 0)"
@@ -16,10 +16,8 @@ then
 fi
 if ! gcc -D_FORTIFY_SOURCE=2 -DSOLARDVERSION=\"`git describe --tag`\" -Wall -Wno-unused-result -O3 -o solard solard.c; then
 	echo "$(tput setaf 1)$(tput setab 7)ERROR: Compilation failed!$(tput sgr 0)"
-	exit 3
 else
-	echo "$(tput setaf 2)Success.$(tput sgr 0)"
-	echo "$(tput setaf 2)Compile time and date: $(tput setab 7)`date`$(tput sgr 0)"
+	echo "$(tput setaf 2)Successfully compiled solard version $(tput smso)`git describe --tag`$(tput smso) at: $(tput setaf 4)`date`$(tput sgr 0)"
 fi
 
 #EOF
