86995e26465e9693c565795b1825c804505a63ba
mrpetrov <>
2019-04-04 20:30:35 +0300

    One more build.sh improvement

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Checking if solard.c is present..."
+SGV=`git describe --tag`
+echo "$(tput setaf 5)Starting compilation...$(tput sgr 0)"
+echo "Checking if $(tput setaf 3)solard.c$(tput sgr 0) is present..."
 if [ ! -e solard.c ]
 then
     echo "$(tput setaf 1)$(tput setab 7)Refusing to start from non-source-root dir...$(tput sgr 0)"
@@ -14,10 +16,10 @@ then
     rm solard
 	echo "$(tput setaf 3)Previous compile result was still present. It is now removed.$(tput sgr 0)"
 fi
-if ! gcc -D_FORTIFY_SOURCE=2 -DSOLARDVERSION=\"`git describe --tag`\" -Wall -Wno-unused-result -O3 -o solard solard.c; then
+if ! gcc -D_FORTIFY_SOURCE=2 -DSOLARDVERSION=\"$SGV\" -Wall -Wno-unused-result -O3 -o solard solard.c; then
 	echo "$(tput setaf 1)$(tput setab 7)ERROR: Compilation failed!$(tput sgr 0)"
 else
-	echo "$(tput setaf 2)Successfully compiled solard version $(tput smso)`git describe --tag`$(tput rmso) at: $(tput setaf 4)`date`$(tput sgr 0)"
+	echo "$(tput setaf 2)$(tput smso)Successfully$(tput rmso) compiled solard version $(tput smso)$SGV$(tput rmso) at: $(tput smso)`date`$(tput sgr 0)"
 fi
 
 #EOF
