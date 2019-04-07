4f7ac0799720b58d51c83332520f0ad6be053dc3
mrpetrov <mrpetrov@users.noreply.github.com>
2019-04-07 14:14:02 +0300

    Smarten-up build.sh
    We now preserve previous compile result, and only remove it if current compile is successful;
    The program version string is smartly generated: if its a standalone compile - version is like STANDALONE-2019-04-08--12:33:55;
    Also, build.sh is a lot more adaptable now that it uses variables...

--- build.sh
+++ build.sh
@@ -1,25 +1,40 @@
 #!/bin/bash
-
-SGV=`git describe --tag`
-echo "$(tput setaf 5)Starting compilation...$(tput sgr 0)"
-echo "Checking if $(tput setaf 3)solard.c$(tput sgr 0) is present..."
-if [ ! -e solard.c ]
+daemon_name=solard
+echo "$(tput setaf 3)Starting $(tput setaf 6)$daemon_name.c$(tput setaf 3) compilation...$(tput sgr0)"
+if [ ! -e $daemon_name.c ]
 then
-    echo "$(tput setaf 1)$(tput setab 7)Refusing to start from non-source-root dir...$(tput sgr 0)"
-    echo "$(tput setaf 1)$(tput setab 7)ERROR: No solard.c found in $(tput smso)`pwd`$(tput rmso)!$(tput sgr 0)"
+    echo "$(tput setaf 7)$(tput setab 1)ERROR: No $(tput smso) $daemon_name.c $(tput rmso) found in current working dir: "\
+             "$(tput smso) `pwd` $(tput rmso)!!!$(tput sgr0)"
     exit 2
 fi
-echo "$(tput setaf 2)OK.$(tput sgr 0)"
+echo "$(tput setaf 2)Source file found!$(tput sgr0)"
 
-if [ -e solard ]
+git describe --tag 1>/dev/null 2>&1
+if (( $? > 0 ))
 then
-    rm solard
-	echo "$(tput setaf 3)Previous compile result was still present. It is now removed.$(tput sgr 0)"
-fi
-if ! gcc -D_FORTIFY_SOURCE=2 -DPGMVER=\"$SGV\" -Wall -Wno-unused-result -O3 -o solard solard.c; then
-	echo "$(tput setaf 1)$(tput setab 7)ERROR: Compilation failed!$(tput sgr 0)"
+    daemon_ver="STANDALONE-`date '+%F--%T'`"
+    echo "Working out version number to use... $(tput setaf 3)CUSTOM$(tput sgr0)."
 else
-	echo "$(tput setaf 2)$(tput smso)Successfully$(tput rmso) compiled solard version $(tput smso)$SGV$(tput rmso) at: $(tput smso)`date`$(tput sgr 0)"
+    daemon_ver=`git describe --tag`
+    echo "Working out version number to use... $(tput setaf 3)GIT-TAG based$(tput sgr0)."
 fi
 
+if [ -e $daemon_name ]
+then
+#    echo "$(tput setaf 3)Previous compile result: still present.$(tput sgr0)"
+    mv $daemon_name $daemon_name.prev
+#    echo "$(tput setaf 3)Previous compile result: renamed for now.$(tput sgr0)"
+fi
+
+gcc -D_FORTIFY_SOURCE=2 -DPGMVER=\"$daemon_ver\" -Wall -Wno-unused-result -O3 -o $daemon_name $daemon_name.c
+if (( $? > 0 ))
+then
+    mv $daemon_name.prev $daemon_name
+    echo "$(tput setaf 3)Previous compile result: RESTORED.$(tput sgr0)"
+    echo "$(tput setaf 7)$(tput setab 1)ERROR: Compilation failed!$(tput sgr0)"
+else
+    rm $daemon_name.prev
+    echo "$(tput setaf 3)Previous compile result: removed.$(tput sgr0)"
+    echo "$(tput setaf 2)$(tput smso)Compilation SUCCESS!$(tput rmso)$(tput sgr0)"
+fi
 #EOF
